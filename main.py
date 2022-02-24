ab4535329cff962d03ebdaf753293e503a0bf341
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-02-24 08:50:44 -0400

    Test

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ import requests  # noqa We are just importing this to prove the dependency insta
 
 
 def main():
-    translations_path = os.environ["INPUT_TRANSLATIONS_PATH"]
-    only_languages = os.environ["INPUT_ONLY_LANGUAGES"]
+    translations_path = os.environ["INPUT_PATH"]
     token = os.environ["INPUT_TOKEN"]
-    min_coverage = os.environ["INPUT_MIN_COVERAGE"]
 
     progress = f"""
         translations_path = {translations_path}
-        only_languages = {only_languages}
         token = {token}
-        min_coverage = {min_coverage}
     """
 
     print(f"::set-output name=progress::{progress}")
