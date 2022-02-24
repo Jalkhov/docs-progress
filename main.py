40a9c528e018f35b47441e6f301e987c96a2c608
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-02-24 08:06:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import requests  # noqa We are just importing this to prove the dependency insta
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    translations_path = os.environ["INPUT_TRANSLATIONS_PATH"]
+    only_languages = os.environ["INPUT_ONLY_LANGUAGES"]
+    token = os.environ["INPUT_TOKEN"]
+    min_coverage = os.environ["INPUT_MIN_COVERAGE"]
 
-    my_output = f"Hello {my_input}"
-
-    print(f"::set-output name=myOutput::{my_output}")
+    print(f"::set-output name=translations_path::{translations_path}")
+    print(f"::set-output name=only_languages::{only_languages}")
+    print(f"::set-output name=token::{token}")
+    print(f"::set-output name=min_coverage::{min_coverage}")
 
 
 if __name__ == "__main__":
