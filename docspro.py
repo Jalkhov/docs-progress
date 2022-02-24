511f5135ea039d91cfdd592b705103e2615aeaca
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-02-24 13:23:18 -0400

    Update docspro.py

--- docspro.py
+++ docspro.py
@@ -32,6 +32,7 @@ class DocsPro(object):
     def __walk_files(self):
         files = self.po_files
         for file in files:
+            print(file)
             po = polib.pofile(file)
             obsoletes = len(po.obsolete_entries())
             msgs_data = self.__msgs_data(po, obsoletes)
@@ -46,7 +47,6 @@ class DocsPro(object):
     def translated(self, fix=True):
         self.__get_po_files()
         self.__walk_files()
-        print("HEREEE!", self.translatedMessageCount, self.totalMessages)
         total = self.__total()
 
         return "%.2f" % (total) if fix else str(total)
