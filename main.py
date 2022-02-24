7af7871053352a2c5b6005a64a9ba1676206e987
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-02-24 09:13:47 -0400

    Test

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import requests  # noqa We are just importing this to prove the dependency insta
 def main():
     translations_path = os.environ["INPUT_PATH"]
     print(translations_path)
-    #progress = f"""
-    #    translations_path = {translations_path}
-    #"""
+    # progress = f"""
+    #     translations_path = {translations_path}
+    # """
     progress = '100%'
     print(f"::set-output name=progress::{progress}")
 
