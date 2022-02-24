ebcbcca1b2f378e67e2743488f79bfe2862c7700
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-02-24 10:39:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ import requests
 
 def main():
     translations_path = os.environ["INPUT_PATH"]
-    # print(translations_path)
-    # progress = f"""
-    #     translations_path = {translations_path}
-    # """
-    progress = '100%'
     print(f"::set-output name=progress::{translations_path}")
 
 
