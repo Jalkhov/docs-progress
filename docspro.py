0c5fa418ba032dd0da6cf87b4ef17b25968079d7
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2022-02-24 22:23:31 -0400

    Improvements

--- docspro.py
+++ docspro.py
@@ -3,27 +3,39 @@ import os
 import polib
 from pprint import pprint
 
+def parser(list):
+    """
+    Convert comma separated items
+    to list
+    """
+    split = list.split(',')
+    out = [x.strip() for x in split]
+
+    return out
 
 class DocsPro(object):
     """docstring for DocsPro"""
-    def __init__(self, path):
+    def __init__(self, path, multilang=False, ignore_langs=''):
         super(DocsPro, self).__init__()
+        self.options = {'multilang':multilang,
+                        'ignore_langs':parser(ignore_langs)}
         self.path = path
-        self.PoFiles = []
-        self.translatedStrings = 0
-        self.totalStrings = 0
+        # self.PoFiles = []
         # self.warnings = []
 
 
-    def __get_PoFiles(self):
+    def __get_PoFiles(self, dir):
         """
         Get all .po files and absolute paths
         """
-        for root, dirs, files in os.walk(self.path):
+        pofiles = []
+        for root, dirs, files in os.walk(dir):
             for file in files:
                 if file.endswith(".po"):
                     file_path = os.path.join(root, file)
-                    self.PoFiles.append(file_path)
+                    pofiles.append(file_path)
+
+        return pofiles
 
 
     def __msgs_data(self, po, obsoletes):
@@ -36,36 +48,71 @@ class DocsPro(object):
         return (translatedStrings, totalStrings)
 
 
-    def __walk_files(self):
+    def __list_langs(self):
+        return os.listdir(self.path)
+
+
+    def __remove_ignored_files(self, pofiles, ignored_langs):
         """
-        Walk all .po files and store global total
-        translated and total strings
+        Remove files with ignored langs in path
         """
-        files = self.PoFiles
-        for file in files:
+        for_remove = []
+        for lang in ignored_langs:
+            lang_match = [file for file in pofiles if f"/{lang}/" in file]
+            for_remove.extend(lang_match)
+
+        return list(set(pofiles) - set(for_remove))
+
+    def __total(self, pofiles):
+        """
+        Calculate final lang total progress
+        """
+        translatedStrings = 0
+        totalStrings = 0
+
+        for file in pofiles:
             po = polib.pofile(file)
             obsoletes = len(po.obsolete_entries())
             msgs_data = self.__msgs_data(po, obsoletes)
-            self.translatedStrings += msgs_data[0]
-            self.totalStrings += msgs_data[1]
+            translatedStrings += msgs_data[0]
+            totalStrings += msgs_data[1]
 
+        total = (translatedStrings / totalStrings) * 100
 
-    def __total(self):
-        """
-        Calculate final total progress
-        """
-        return (self.translatedStrings / self.totalStrings) * 100
+        return "%.2f" % (total)
 
 
     def translated(self, fix=True):
-        self.__get_PoFiles()
-        self.__walk_files()
+        ignored_langs = self.options['ignore_langs']
 
-        total = self.__total()
+        if self.options['multilang']:
+            target_langs = self.__list_langs()
+            target_langs = [x for x in target_langs if x not in ignored_langs]
+            langs_data = {}
 
-        return "%.2f" % (total) if fix else str(total)
+            for lang in target_langs:
+                lang_dir = os.path.join(self.path, lang)
+                lang_files = self.__get_PoFiles(lang_dir)
+                lang_progress = self.__total(lang_files)
+                langs_data[lang] = lang_progress
+
+            return langs_data
+
+        else:
+            pofiles = self.__get_PoFiles(self.path)
+            pofiles = self.__remove_ignored_files(pofiles, ignored_langs)
+            total = self.__total(pofiles)
+
+            return total
+
+def main():
+    path = 'docs/locales'
+    dp = DocsPro(path)
+    d = dp.translated()
+    pprint(d)
+
+if __name__ == '__main__':
+    main()
 
 
-path = 'docs/locales'
-dp = DocsPro(path)
-d = dp.translated()
+
